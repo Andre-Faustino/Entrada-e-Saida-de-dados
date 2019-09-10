e33cb491b52c7815fda23940e5b57e6338795284
Andre-Faustino <andre.domingues.faustino@hotmail.com>
2019-09-10 18:26:15 -0200

    Projeto criado

---
+++ src/course/Program.java
@@ -0,0 +1,27 @@
+package course;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Students;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.ENGLISH);
+		Scanner sc = new Scanner(System.in);
+			
+		
+		sc.close();
+		
+		float x = 0.1f + 0.3f;
+		
+		System.out.println(x);
+		
+	}
+	
+	
+	
+
+}
